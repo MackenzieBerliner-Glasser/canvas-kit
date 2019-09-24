8e87cd446da94cfdfb2f417f3d90cfce0896d864
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-24 16:22:06 +0000

    Deploy Workday/canvas-kit to github.com/Workday/canvas-kit.git:gh-pages

---
+++ precache-manifest.c0e5188da50b922f200b3380a12fbf9c.js
@@ -0,0 +1,23 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59eb566334aca479a153b74d4f1b9a15",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ddeea4b72e5a5dbb1ecf.bundle.js"
+  },
+  {
+    "revision": "973ed55638df8a1164575939c93dd5e1",
+    "url": "modules/header/react/static/workday-bg.jpg"
+  },
+  {
+    "url": "runtime~main.ddeea4b72e5a5dbb1ecf.bundle.js"
+  },
+  {
+    "revision": "09a53caac8d0128f5b1f4509b01583c4",
+    "url": "static/media/workday-bg.09a53caa.jpg"
+  },
+  {
+    "url": "vendors~main.ddeea4b72e5a5dbb1ecf.bundle.js"
+  }
+]);
